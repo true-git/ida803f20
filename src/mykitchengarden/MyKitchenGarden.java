b3f1c247933b1557832a69d0ff5e47e85d8d5545
Deadlyfungus <41171043+Deadlyfungus@users.noreply.github.com>
2020-04-14 10:18:07 +0200

    testing

--- src/mykitchengarden/MyKitchenGarden.java
+++ src/mykitchengarden/MyKitchenGarden.java
@@ -4,8 +4,12 @@
  * and open the template in the editor.
  */
 package mykitchengarden;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 /**
@@ -15,20 +19,32 @@ import javafx.stage.Stage;
  */
 public class MyKitchenGarden extends Application {
     
+     private Button button_my_plants;
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Button to change page
+        Button button_my_plants = new Button();
+        button_my_plants.setText("Mine planter");
+        
+        
         DatabaseConnection dbc = new DatabaseConnection();
         dbc.executeQuery("Select * from plants");
         while(dbc.myRs.next()) {
             System.out.println(dbc.myRs.getString("common_name") + ", " + dbc.myRs.getString("latin_name") +
                     ", " + dbc.myRs.getString("climate_zone"));
         }
-        Scene scene = new Scene(new StackPane(), 900, 600);
+        //The BorderPane is created called layout
+        BorderPane layout = new BorderPane();
+        layout.setCenter(button_my_plants);
+        
+        //scene is created and put inside the primaryStage
+        Scene menuScene = new Scene(layout, 357, 667);
         //scene.getStylesheets().add("/mykitchengarden/layoutstyles.css");
-                
         primaryStage.setTitle("PlantLet");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(menuScene);
         primaryStage.show();
+                 
     }
 
     /**

98f59f570267db2803d44a400b0dcfc4ddab7013
true-git <61357351+true-git@users.noreply.github.com>
2020-05-04 08:38:27 +0200

    Add files via upload

--- DB_P8_Script.sql
+++ DB_P8_Script.sql
@@ -4,16 +4,30 @@ GRANT ALL PRIVILEGES ON *.* TO 'kitchengardener'@'localhost';
 CREATE DATABASE IF NOT EXISTS kitchengarden;
 USE kitchengarden;
 
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS plants;
 
+CREATE TABLE users (
+  user_id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(64) DEFAULT NULL,
+  email varchar(64) DEFAULT NULL,
+  pass varchar (64) DEFAULT NULL,
+  city varchar(64) DEFAULT NULL,
+  PRIMARY KEY (user_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+INSERT INTO users (username, email, pass, city) VALUES
+('Testperson', 'testperson@plantlet.dk', 'test', 'Testkøbing');
+
 CREATE TABLE plants (
-  id int(11) NOT NULL AUTO_INCREMENT,
+  latin_name varchar(64) NOT NULL,
   common_name varchar(64) DEFAULT NULL,
-  latin_name varchar(64) DEFAULT NULL,
+  presentation blob DEFAULT NULL,
   climate_zone int DEFAULT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (latin_name)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 INSERT INTO plants VALUES
-(1, 'dandelion', 'dandelionius', 5),
-(2, 'sweet potato', 'sweetus potatous', 4);
+('dandelionius', 'dandelion', 'præsentation', 5),
+('sweetus potatous', 'sweet potato', 'præsentation', 4);
+

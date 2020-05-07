584dfe54c00c48ae653ffc59ef68ad39c14f90e6
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-05-07 09:41:12 +0200

    Merged 2 scripts

--- DB_P8_Script.sql
+++ DB_P8_Script.sql
@@ -9,36 +9,36 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS plantlibrary;
 
 CREATE TABLE users (
-  user_id int(11) AUTO_INCREMENT not null, 
-  firstname varchar(64) DEFAULT NULL,
-  lastname varchar(64) DEFAULT NULL,
-  email varchar(64) DEFAULT NULL,
-  pass varchar (64) DEFAULT NULL,
-  city varchar(64) DEFAULT NULL, 
+  user_id int(11) NOT NULL auto_increment,
+  firstname varchar(64) NOT NULL,
+  lastname varchar(64) NOT NULL,
+  email varchar(64) NOT NULL,
+  pass varchar (64) NOT NULL,
+  city varchar(64) NOT NULL, 
   gender ENUM ('male', 'female', 'other') NOT NULL,
-  datofbirth DATE NOT NULL, 
+  dateofbirth DATE NOT NULL, 
   isLoggedIn BOOLEAN NOT NULL,
   PRIMARY KEY (user_id)
   
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO users VALUES 
-(1, 'Test', 'Jensen', 'testperson@plantlet.dk', 'salt', 'Testkøbing', 'other', '1985-10-12', false),
-(2, 'Test2', 'Hansen', 'test2@email.com', 'peber', 'Nykøbing', 'male', '2000-01-01', false);
+INSERT INTO users (firstname, lastname, email, pass, city, gender, dateofbirth, isLoggedIn) VALUES 
+('Test', 'Jensen', 'testperson@plantlet.dk', 'salt', 'Testkøbing', 'other', '1985-10-12', false),
+('Test2', 'Hansen', 'test2@email.com', 'peber', 'Nykøbing', 'male', '2000-01-01', false);
  
  
 CREATE TABLE plantlibrary (
-  plant_id int auto_increment not null,
+  plant_id int NOT NULL auto_increment,
   latin_name varchar(64) NOT NULL,
   common_name varchar(64) DEFAULT NULL,
-  
   climate_zone int DEFAULT NULL,
+  presentation blob DEFAULT NULL,
   PRIMARY KEY (plant_id) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO plantlibrary VALUES
-(1, 'dandelion', 'dandelionius', 5),
-(2, 'sweet potato', 'sweetus potatous', 4);
+INSERT INTO plantlibrary (latin_name, common_name, climate_zone) VALUES
+('Dandelionius', 'dandelion', 5),
+('Sweetos potatos', 'Sweet potatos', 4);
 
 
 CREATE TABLE registred_plants (
@@ -46,7 +46,7 @@ CREATE TABLE registred_plants (
 	user_id int(11) not null,
     plant_id int,
     state ENUM ('frø', 'spirer'),
-    dateof DATE NOT NULL,
+    plantedondate DATE NOT NULL,
     foreign key (user_id) references users (user_id),
     foreign key (plant_id) references plantlibrary (plant_id)
 );

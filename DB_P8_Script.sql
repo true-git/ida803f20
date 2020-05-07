6108d48f611b04ba30793556daccee4d9b5865a7
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-05-07 09:10:07 +0200

    New working ySQL script
    Added new tables and updated users table.

--- DB_P8_Script.sql
+++ DB_P8_Script.sql
@@ -4,30 +4,52 @@ GRANT ALL PRIVILEGES ON *.* TO 'kitchengardener'@'localhost';
 CREATE DATABASE IF NOT EXISTS kitchengarden;
 USE kitchengarden;
 
+DROP TABLE if EXISTS registred_plants;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS plants;
+DROP TABLE IF EXISTS plantlibrary;
 
 CREATE TABLE users (
-  user_id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(64) DEFAULT NULL,
+  user_id int(11) AUTO_INCREMENT not null, 
+  firstname varchar(64) DEFAULT NULL,
+  lastname varchar(64) DEFAULT NULL,
   email varchar(64) DEFAULT NULL,
   pass varchar (64) DEFAULT NULL,
-  city varchar(64) DEFAULT NULL,
+  city varchar(64) DEFAULT NULL, 
+  gender ENUM ('male', 'female', 'other') NOT NULL,
+  datofbirth DATE NOT NULL, 
+  isLoggedIn BOOLEAN NOT NULL,
   PRIMARY KEY (user_id)
+  
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO users (username, email, pass, city) VALUES
-('Testperson', 'testperson@plantlet.dk', 'test', 'Testkøbing');
-
-CREATE TABLE plants (
+INSERT INTO users VALUES 
+(1, 'Test', 'Jensen', 'testperson@plantlet.dk', 'salt', 'Testkøbing', 'other', '1985-10-12', false),
+(2, 'Test2', 'Hansen', 'test2@email.com', 'peber', 'Nykøbing', 'male', '2000-01-01', false);
+ 
+ 
+CREATE TABLE plantlibrary (
+  plant_id int auto_increment not null,
   latin_name varchar(64) NOT NULL,
   common_name varchar(64) DEFAULT NULL,
-  presentation blob DEFAULT NULL,
+  
   climate_zone int DEFAULT NULL,
-  PRIMARY KEY (latin_name)
+  PRIMARY KEY (plant_id) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO plants VALUES
-('dandelionius', 'dandelion', 'præsentation', 5),
-('sweetus potatous', 'sweet potato', 'præsentation', 4);
+INSERT INTO plantlibrary VALUES
+(1, 'dandelion', 'dandelionius', 5),
+(2, 'sweet potato', 'sweetus potatous', 4);
+
+
+CREATE TABLE registred_plants (
+
+	user_id int(11) not null,
+    plant_id int,
+    state ENUM ('frø', 'spirer'),
+    dateof DATE NOT NULL,
+    foreign key (user_id) references users (user_id),
+    foreign key (plant_id) references plantlibrary (plant_id)
+);
+
+
 

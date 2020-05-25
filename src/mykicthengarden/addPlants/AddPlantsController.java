4bf444c7a462c37387fcac32e834154dcd170889
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-05-25 12:14:09 +0200

    added commenting

--- src/mykicthengarden/addPlants/AddPlantsController.java
+++ src/mykicthengarden/addPlants/AddPlantsController.java
@@ -23,9 +23,11 @@ public class AddPlantsController {
     private String state;
     public int user_id;
     private int plant_id;
-
+    
+    
     public AddPlantsController(AddPlantsModel addPlantsModel, AddPlantsView addPlantsView, Stage stage) {
-     
+        
+        //get the back button from the view and change the scene
         addPlantsView.getBack_button().setOnAction(e -> {
             MyPlantsModel myPlantsModel = new MyPlantsModel(addPlantsModel.user_id, addPlantsModel.userName);
             MyPlantsView myPlantsView = new MyPlantsView(addPlantsModel.user_id);
@@ -33,14 +35,17 @@ public class AddPlantsController {
             MyPlantsController myPlantsController = new MyPlantsController(myPlantsModel, myPlantsView, stage);
         });
         
+        //get the OK button from the view and 
         addPlantsView.getOk_button().setOnAction(e -> {
             
             try {
-                //Getting the input of the textfield
+                //Getting the input of the textfield from the view
                 searchTerm = addPlantsView.getInput();
                 
                 //Check if the user has entered a valid search term, and return the result.
+                //if no searchterm - print to the console
                 if(searchTerm.equals("")){
+                    // (should be an error message to the user)
                     System.out.println("Please select a plant to add.");
                 } else {
                     //If the searchterm is valid, check the database for the plant
@@ -53,8 +58,10 @@ public class AddPlantsController {
                         user_id = addPlantsModel.user_id;
                         
                         addPlantsModel.registrePlant(user_id, plant_id, date, state);
+                        //Should be a message box to the user
                         System.out.println("Plant registred!");                        
                 } else {
+                        //Should be a messagebox to the user
                         System.out.println("We dont have that plant in our database. Please try another plant.");
                     }
                 }   

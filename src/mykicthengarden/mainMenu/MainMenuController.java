8bbc55a453f7dcdc8affe874ddaa494890787ee3
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-05-25 12:36:24 +0200

    added commenting

--- src/mykicthengarden/mainMenu/MainMenuController.java
+++ src/mykicthengarden/mainMenu/MainMenuController.java
@@ -24,6 +24,7 @@ public class MainMenuController {
     
     public MainMenuController(MainMenuModel mainMenuModel, MainMenuView mainMenuView, Stage stage) {
         
+        //get the my_plants_button from the view, and change the scene to myPlantsView
         mainMenuView.getButton_my_plants().setOnAction(e -> {
             MyPlantsModel myPlantsModel = new MyPlantsModel(mainMenuModel.user_id, mainMenuModel.userName);
             MyPlantsView myPlantsView = new MyPlantsView(mainMenuModel.user_id);
@@ -31,6 +32,8 @@ public class MainMenuController {
             MyPlantsController myPlantsController = new MyPlantsController(myPlantsModel, myPlantsView, stage);  
         });
         
+        
+        //get the plantLibrary_button from the view, and change the scene to plantLibrary
         mainMenuView.getButton_plant_library().setOnAction(e -> {
             System.out.print("test");
             PlantLibraryModel plantLibraryModel = new PlantLibraryModel(mainMenuModel.user_id, mainMenuModel.userName);
@@ -39,6 +42,8 @@ public class MainMenuController {
             PlantLibraryController plantLibraryController = new PlantLibraryController(plantLibraryModel, plantLibraryView, stage);      
         });
         
+        
+        //get the askExperts_button from the view, and change the scene to askExpertsView
         mainMenuView.getButton_ask_expert().setOnAction(e ->{
             System.out.print("test");
             AskExpertsModel askExpertsModel = new AskExpertsModel(mainMenuModel.user_id, mainMenuModel.userName);
@@ -47,18 +52,25 @@ public class MainMenuController {
             AskExpertsController askExpertsController = new AskExpertsController(askExpertsModel, askExpertsView, stage);        
         });
         
+        //testing the button
         mainMenuView.getMenu_item_log_out().setOnAction(e -> {
             System.out.println("log out");
         });
         
+        
+        //testing the button
         mainMenuView.getMenu_item_settings().setOnAction(e -> {
             System.out.println("settings");
         });
         
+        
+        //testing the button
         mainMenuView.getButton_to_do_calendar().setOnAction(e ->{
             System.out.print("test");
         });
         
+        
+        //testing the button
         mainMenuView.getButton_weather().setOnAction(e -> {
             System.out.print("test"); 
         });
